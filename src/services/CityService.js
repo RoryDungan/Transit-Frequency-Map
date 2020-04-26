c5b3378839bd8b8636de27b2a498f274b4b91fd5
Rory Dungan <rorydungan@gmail.com>
2020-04-26 20:58:48 +1000

    Use relative paths in CityService

--- src/services/CityService.js
+++ src/services/CityService.js
@@ -2,7 +2,7 @@ import { decodeAsync } from '@msgpack/msgpack'
 
 export default function (fetch) {
   const getCityData = async (cityId, time, frequency) => {
-    const url = `/cities/${cityId}-${time}-${frequency}mins.msg`
+    const url = `cities/${cityId}-${time}-${frequency}mins.msg`
     const res = await fetch(url)
     if (!res.ok) {
       throw new Error(`Tried to GET ${url}, returned status ${res.status}`)
