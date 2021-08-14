cd17aa7d9ca411b6eed42ce6bf1a481810377cfd
Rory Dungan <rorydungan@gmail.com>
2021-08-14 23:57:07 +1000

    Move zoom control to the right

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { MapContainer, TileLayer, GeoJSON } from 'react-leaflet'
+import { MapContainer, TileLayer, GeoJSON, ZoomControl } from 'react-leaflet'
 
 import './MapContainer.css'
 import 'leaflet/dist/leaflet.css'
@@ -11,7 +11,8 @@ const { Content } = Layout
 export default function MyMapContainer(props) {
   return (
     <Content style={{display: 'flex'}}>
-      <MapContainer className="Map" zoom={12} center={props.position}>
+      <MapContainer className="Map" zoom={12} zoomControl={false} center={props.position}>
+        <ZoomControl position="topright"/>
         <TileLayer
           attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
