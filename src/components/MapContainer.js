84d1f2cc2a50694f178328d2dace5776a8690207
Rory Dungan <rorydungan@gmail.com>
2021-08-15 00:14:15 +1000

    Fix issue where map wouldn't move when selecting a new city

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
-import { MapContainer, TileLayer, GeoJSON, ZoomControl } from 'react-leaflet'
+import { MapContainer, TileLayer, GeoJSON, ZoomControl, useMap } from 'react-leaflet'
 
 import './MapContainer.css'
 import 'leaflet/dist/leaflet.css'
@@ -8,10 +8,21 @@ import { Layout, Spin } from 'antd'
 
 const { Content } = Layout
 
+function MapController(props) {
+  const map = useMap()
+
+  useEffect(() => {
+    map.setView(props.center)
+  }, [props.center])
+  return <div/>
+}
+
 export default function MyMapContainer(props) {
+  console.log('Map center: ' + props.position)
   return (
     <Content style={{display: 'flex'}}>
       <MapContainer className="Map" zoom={12} zoomControl={false} center={props.position}>
+        <MapController center={props.position} />
         <ZoomControl position="topright"/>
         <TileLayer
           attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
